f680912e9b7f1af498f9c3833001b57ce020e31a
003084-K <41455075+003084-K@users.noreply.github.com>
2021-04-07 14:05:37 -0700

    organizing

--- eeg_quick_test/eeg_quick_test.ino
+++ eeg_quick_test/eeg_quick_test.ino
@@ -1,9 +1,9 @@
 elapsedMicros SinceStart;
 elapsedMicros LastScan;
 
-unsigned long RunTime = 1000000;  // in micros
+unsigned long RunTime = 2500000;  // in micros
 unsigned long ScanSpace = 250;   // in micros
-unsigned long n_samples = RunTime/ScanSpace;
+unsigned long n_samples = RunTime / ScanSpace;
 unsigned long starttime;
 unsigned long sample = 1;
 unsigned long endtime;
@@ -17,11 +17,11 @@ unsigned long EEG_val;
 
 void setup() {
 
-Serial.begin(9600);
+  Serial.begin(9600);
 
-//  pinMode(EEG,INPUT_DISABLE);
+  //  pinMode(EEG,INPUT_DISABLE);
 
-  delay(5000);
+  delay(2000);
 
   starttime = micros();
   endtime = starttime + RunTime;
@@ -30,26 +30,27 @@ Serial.begin(9600);
 
 void loop() {    // only takes 17-18us to loop this
 
-//  if (micros() < endtime) {
+  //  if (micros() < endtime) {
 
-      if (LastScan >= ScanSpace){
-      if (sample <= n_samples) {
-        sample = sample+1;
-        LastScan = 0;
+  if (LastScan >= ScanSpace) {
+    if (sample <= n_samples) {
+      sample = sample+1;
       
-    //    delay(1);
-//        t1 = micros();
-        EEG_val = analogRead(EEG);
+      LastScan = 0;
 
-//      t2 = micros();
-//      tottime = t2-t1;
-//      Serial.print("TIMEEEEE:  ");
-//      Serial.println(tottime);
-        Serial.println(EEG_val);
-//        Serial.println(micros());
+      //    delay(1);
+      //        t1 = micros();
+      EEG_val = analogRead(EEG);
 
-    
-      }
+      //      t2 = micros();
+      //      tottime = t2-t1;
+      //      Serial.print("TIMEEEEE:  ");
+      //      Serial.println(tottime);
+      Serial.println(EEG_val);
+      //        Serial.println(micros());
+
+
+    }
 
   }
 
