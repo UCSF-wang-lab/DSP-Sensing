4a9c32947a60e3b494c8df817e3efed9b36ab63c
003084-K <41455075+003084-K@users.noreply.github.com>
2021-06-15 23:42:17 -0700

    fixes to analog write and files for next patient

--- Cap_Touch_Reading/Cap_Touch_Reading.ino
+++ Cap_Touch_Reading/Cap_Touch_Reading.ino
@@ -1,36 +1,37 @@
 
-const int Cap1 = 1;
+const int Cap1 = 0;
 long Cap1_val;
 long Cap1_report;
 
-const int lower_in = 2800;
-const int upper_in = 3800;
+const int lower_in = 3800;
+const int upper_in = 4800;
 
 const int lower_out = 0;
-const int upper_out = 1023;
+const int upper_out = 1000;
 
-unsigned long RunTime = 10000;
+unsigned long RunTime = 1000000;
 unsigned long time1;
 unsigned long time2;
 unsigned long tottime;
 
 void setup() {  
   // put your setup code here, to run once:
-  Serial.begin(1000000);
-  delay(3);
+//  Serial.begin(1000000);
+  analogWriteResolution(10);
+  delay(12);
 }
 
 void loop() {
-  if (millis() < RunTime) {
+//  if (millis() < RunTime) {
 
-    time1 = micros();
+//    time1 = micros();
     Cap1_val = touchRead(Cap1);
-    time2 = micros();
-    tottime = time2 - time1;
+//    time2 = micros();
+//    tottime = time2 - time1;
 
 //    Serial.print(Cap1_val);
-    Serial.println(Cap1_val);
-//    Serial.println(tottime);
+//    Serial.println(Cap1_val);
+//    Serial.println(tott1ime);
 
   Cap1_report = map(Cap1_val,lower_in,upper_in,lower_out,upper_out);
   Cap1_report = constrain(Cap1_report,lower_out,upper_out);
@@ -41,5 +42,5 @@ void loop() {
 
 
 
-  }
+//  }
 }
